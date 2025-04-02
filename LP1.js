b2744a7a00700e7c281cf0f5fa5b1d7202f3dcbb
Supratimdas20 <supratimdaswork@gmail.com>
2025-04-02 22:33:01 +0530

    Learning Day3

--- LP1.js
+++ LP1.js
@@ -1,7 +1,20 @@
+// function waitFor3S(resolve) {
+//     setTimeout(resolve, 3000);
+// }
+// function main() {
+//     console.log("main is called");
+// }
+// waitFor3S(main);
+
+
+
 function waitFor3S(resolve) {
-    setTimeout(resolve, 3000);
+    setTimeout(resolve, 10000);
+}
+function setTimeoutPromisified() {
+    return new Promise(waitFor3S);
 }
 function main() {
     console.log("main is called");
 }
-waitFor3S(main);
+setTimeoutPromisified().then(main);
