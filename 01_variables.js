69b19b729a29bc737858a4acfb9f0140a4223eeb
Supratimdas20 <supratimdaswork@gmail.com>
2025-04-13 22:16:14 +0530

    Learning Day 10

--- 01_variables.js
+++ 01_variables.js
@@ -1,14 +1,21 @@
 const accountId = 1234567890
 let accountEmail = "supratimdas@google.com"
 var accountPassword = "12345"
-// sometime in javascript we ca declare a variable without any keyword (let,var,const) but it is not recommended.
+// sometime in javascript we can
+// declare a variable without any keyword 
+// (let,var,const) but it is not recommended.
 accountCity = "Kolkata"
 
-int accountState;
-// can print variables/constant in a tabular form
-console.table([accountId, accountPassword, accountEmail, accountCity, accountState]);
+let accountState; // if printed will retrun undefined.
+
+// can print variables/constant in a tabular form.
+console.table([accountId, accountPassword, accountEmail, accountCity]);
 
 
-// accountId = 2233344 : will throw error because "accountId" declared as a constant
+// accountId = 2233344 : will throw 
+// error because "accountId" declared as a constant
 console.log(accountId)
 
+/* Prefer not to use var
+because of issue in block scope and functional scope
+*/
