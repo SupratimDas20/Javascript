669406f15a60da0e127944d4a7076aba3f4b1c34
Supratimdas20 <supratimdaswork@gmail.com>
2025-04-23 00:27:55 +0530

    Learning Day 17

--- 01_basics/08_dateAndTime.js
+++ 01_basics/08_dateAndTime.js
@@ -2,3 +2,4 @@ let myDate = new Date();
 console.log(myDate); // current date and time
 console.log(myDate.toString()); // current date and time in string format
 console.log(myDate.toLocaleString()); // current date and time in local format
+console.log(typeof myDate); // object
