d88803ebbdcdbdcbcd489bc2f63ac324d0d64679
Supratimdas20 <supratimdaswork@gmail.com>
2025-04-27 22:50:23 +0530

    Learning Day 21

--- 01_basics/08_dateAndTime.js
+++ 01_basics/08_dateAndTime.js
@@ -12,3 +12,6 @@ console.log(myCreatedDate1); // 23 Jan 2023
 
 let myCreatedDate2 = new Date(2023, 0, 23, 5, 3)
 console.log(myCreatedDate2); // 23 Jan 2023 05:03:00
+
+let myCreatedDate3 = new Date("01-14-2023")
+console.log(myCreatedDate3.toLocaleString());
